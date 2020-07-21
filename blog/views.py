a52587fc0915f971b55feb55ca38ed95da0694e1
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-21 15:07:03 +0530

    ..from django.core.paginator import Paginator, EmptyPage,\                                   PageNotAnInteger

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
 from .models import Post
 def post_list(request):
     posts = Post.published.all()
