cddef4fb69bbb5ec8c40cf0246e1d9b8fe5aeb2d
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-21 15:18:23 +0530

    class-based views

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from .models import Post
+from django.views.generic import ListView
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 def post_list(request):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3) # 3 posts in each page
